f7e52f04d095803f7d43ee89cd76e099f523abdc
KalyaniDakhare <55684049+KalyaniDakhare@users.noreply.github.com>
2019-12-18 10:47:16 +0530

    Add files via upload
    mvc model

---
+++ controller.py
@@ -0,0 +1,19 @@
+from pip._vendor.distlib.compat import raw_input
+
+from model import Person
+import view
+
+def showAll():
+   people_in_db = Person.getAll()
+   return view.showAllView(people_in_db)
+
+def start():
+   view.startView()
+   input = raw_input()
+   if input == 'y':
+      return showAll()
+   else:
+      return view.endView()
+
+if __name__ == "__main__":
+   start()
